f9248d4a88bb9aa0f90f4a5f18ec4e217cbd3586
auliabadruzzaman <azzamaulia.02@gmail.com>
2024-09-22 19:50:53 +0700

    Add files via upload

---
+++ Assets/Scripts/BumperControl.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BumperController : MonoBehaviour
+{
+    public Collider bola;
+    public float multiplier;
+    public Color color;
+
+    
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider == bola)
+        {
+            Rigidbody bolaRig = bola.GetComponent<Rigidbody>();
+            bolaRig.velocity *= multiplier;
+
+            //animate
+            animator.SetTrigger("hit");
+        }
+    }
+}
